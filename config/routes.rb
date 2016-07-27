8007fd35f614ff448f02c1b7c8e4ea9a74ed7b7e
Laleibo <Laleibo@gmail.com>
2016-07-27 09:50:49 -0400

    routes user_url path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 root 'user#index'
 
 post '/user/create'
-get 'show/:id' => 'user#show'
+get 'user/:id' => 'user#show', :as => 'user'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
